380b8995e66f586903447381358fcd02fbb68e2a
David Elliott <david@gradientdevices.com>
2022-03-29 18:12:53 -0700

    improve time complexity for Kth largest

--- src/problems/kth_largest.go
+++ src/problems/kth_largest.go
@@ -1,15 +1,34 @@
 package problems
 
-import (
-	"sort"
-)
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 func KthLargest(k int, initialStream, appendStream []int) []int {
+	h := IntHeap(initialStream)
+	heap.Init(&h)
+
 	results := []int{}
 	for _, e := range appendStream {
-		initialStream = append(initialStream, e)
-		sort.Slice(initialStream, func(i, j int) bool { return initialStream[i] < initialStream[j] })
-		results = append(results, initialStream[len(initialStream)-k])
+		heap.Push(&h, e)
+		for h.Len() > k {
+			heap.Pop(&h)
+		}
+		results = append(results, h[0])
 	}
 	return results
 }
