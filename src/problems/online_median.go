b5bc5d97117cbee649ceebba0bc843a20de347a4
David Elliott <david.elliott@stitchfix.com>
2022-04-02 15:53:05 -0700

    make it go faster

--- src/problems/online_median.go
+++ src/problems/online_median.go
@@ -7,20 +7,33 @@ func OnlineMedian(stream []int) []int {
 	ret := []int{}
 
 	for _, e := range stream {
-		tmp = append(tmp, e)
-		sort.Ints(tmp)
-		l := len(tmp)
-		if l == 1 {
+		if len(tmp) == 0 {
+			tmp = append(tmp, e)
 			ret = append(ret, e)
 			continue
 		}
-		median := 0
-		if l%2 == 0 {
-			median = (tmp[l/2-1] + tmp[l/2]) / 2
+
+		i := sort.SearchInts(tmp, e)
+		if i < len(tmp) {
+			tmp = append(tmp[:i+1], tmp[i:]...)
+			tmp[i] = e
 		} else {
+			tmp = append(tmp, e)
+		}
+
+		median := 0
+
+		switch l := len(tmp); {
+		case l == 2:
+			median = (tmp[0] + tmp[1]) / 2
+		case l%2 == 0:
+			median = (tmp[l/2-1] + tmp[l/2]) / 2
+		default:
 			median = tmp[l/2]
 		}
+
 		ret = append(ret, median)
 	}
+
 	return ret
 }
